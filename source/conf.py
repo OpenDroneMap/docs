064c4ff5e3d3cfc9472875383fcd2c4a38dbf3c3
danbjoseph <danbjoseph@gmail.com>
2018-05-22 14:37:09 -0400

    favicon to resolve #4

--- source/conf.py
+++ source/conf.py
@@ -63,6 +63,8 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
+html_favicon="_static/favicon.ico"
+
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
