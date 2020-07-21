4ad996fee1318b831aac0b2e3943292414771023
danbjoseph <danbjoseph@gmail.com>
2020-07-21 15:23:03 -0400

    set gettext_uuid to True

--- source/conf.py
+++ source/conf.py
@@ -35,6 +35,7 @@ extensions = [
 #For internationalization:
 locale_dirs = ['locale/']
 gettext_compact = False
+gettext_uuid = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
